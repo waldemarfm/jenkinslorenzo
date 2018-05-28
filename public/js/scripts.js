2badc6b67c6e804b4bf0dd94cba2685325cdbe35
Waldemar Figueroa <walde.fm@gmail.com>
2018-05-28 17:20:14 -0400

    Responsive enhancements for mobile/tablet

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,4 +4,20 @@ $(document).ready(function() {
     otherClose: 'a.js-close-featherlight',
   };
 
+  const desktop = window.matchMedia('(min-width: 61.25em)');
+
+  function appendVideo(mq) {
+    const $video = $('.js-jl-hero-video');
+
+    if (mq.matches) {
+      const videoSrc = $video.data('src');
+      const $source = `<source type="video/mp4" src="${ videoSrc }" />`;
+      $video.append($source);
+    } else {
+      $video.find('source').remove();
+    }
+  }
+
+  appendVideo(desktop);
+  desktop.addListener(appendVideo);
 });

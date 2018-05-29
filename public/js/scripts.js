d369da0e1748f86820dcb851da2ebcf7c106ee9b
Waldemar Figueroa <walde.fm@gmail.com>
2018-05-28 22:21:21 -0400

    Add header navigation logic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,6 +4,52 @@ $(document).ready(function() {
     otherClose: 'a.js-close-featherlight',
   };
 
+  /**
+   * Local navigation
+   */
+  var $nav = $('#js-navigation');
+  $nav.localScroll({
+    offset: -72,
+  });
+
+  /**
+   * Navigation active state while scrolling
+   */
+  var $sections = $('.jl-js-section');
+
+  $sections.waypoint({
+    handler: function(dir) {
+      var id = this.element.id;
+
+      if (dir === 'down') {
+        markActive(id);
+      }
+    },
+    offset: '90%',
+  });
+
+  $sections.waypoint({
+    handler: function(dir) {
+      var id = this.element.id;
+
+      if (dir === 'up') {
+        markActive(id);
+      }
+    },
+    offset: function() {
+      return -$(this.element).height() + 100;
+    },
+  });
+
+  // Mark link as selected
+  function markActive(id) {
+    $nav.find('a').removeClass('is-active');
+    $nav.find('a[href="#' + id + '"]').addClass('is-active');
+  }
+
+  /**
+   * Homepage hero
+   */
   const desktop = window.matchMedia('(min-width: 61.25em)');
 
   function appendVideo(mq) {
